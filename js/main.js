032e5f1e80a0b8b41df204d29bc889a87b74794b
Andrey Sorokin <73.anir@gmail.com>
2021-11-26 15:37:23 +0400

    Responsive

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
-const slides = document.querySelectorAll('.slider__slides');
-const dots = document.querySelectorAll('.footer__dot');
+// slider
+const slides = document.querySelectorAll('.slider__slides')
+const dots = document.querySelectorAll('.footer__dot')
 
 function setActive(i) {
 	for (slide of slides)
-		slide.classList.remove('active');
-	slides[i].classList.add('active');
+		slide.classList.remove('active')
+	slides[i].classList.add('active')
 
 	for (dot of dots)
-		dot.classList.remove('active');
-	dots[i].classList.add('active');
+		dot.classList.remove('active')
+	dots[i].classList.add('active')
 }
 
-for(let j = 0; j < dots.length; j++){
-	dots[j].addEventListener('click', function(){
-		setActive(j);
+for (let j = 0; j < dots.length; j++) {
+	dots[j].addEventListener('click', function () {
+		setActive(j)
 	})
 }
+
+// toggle
+const headerToggle = document.querySelector('.header__toggle')
+const headerNavigation = document.querySelector('.header__navigation')
+headerToggle.addEventListener('click', () => {
+	headerToggle.classList.toggle('_active')
+	headerNavigation.classList.toggle('_active')
+})
